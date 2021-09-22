da7aa07a8c3270f16b41819470f04051fb0e7486
sushant2931 <sushantlad8888@gmail.com>
2021-09-22 21:54:14 +0530

    Added : Welcome In User Registration Program Using Lambda Function

---
+++ src/com/bridgelabz/userregistrationlambda/UserRegistrationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.userregistrationlambda;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("...... Welcome On User Registration Program Using Lambda Function.....");
+    }
+}
